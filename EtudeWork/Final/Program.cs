a09bd121aad77950c05ff8e920999618e2db04a9
Anna Chirkova <anna.davainis@gmail.com>
2022-09-20 14:47:48 +0500

    Do task in prorgamm

---
+++ EtudeWork/Final/Program.cs
@@ -0,0 +1,39 @@
+﻿string [] CreateStringArray (int size)
+{
+    string [] newArray = new string [size];
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write ($"Введите {i+1} значение массива: ");
+        newArray[i] = Console.ReadLine();        
+    }
+    return newArray;
+}
+
+void ShowArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void ThreeSimvolosArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        Console.Write(array[i] + " ");
+    }  
+}
+
+int size=3;
+
+string [] array = CreateStringArray(size);
+Console.WriteLine("Input simvols: "); 
+ShowArray(array);
+
+Console.WriteLine("Значения символов в массиве, длина которых < или = 3 :");
+ThreeSimvolosArray(array);
+
+Console.WriteLine();
