1aaa984d50fc1a4559cfc0bfb64dc0329536a141
Anna Chirkova <anna.davainis@gmail.com>
2022-12-08 23:02:18 +0500

    do 2 work+2

--- python/HW6/HomeWork6.py
+++ python/HW6/HomeWork6.py
@@ -26,17 +26,21 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-n = input('Введите вещественное число: ')
-sum = sum(map(int, n.replace('.', '')))
-print (sum)
+# n = input('Введите вещественное число: ')
+# sum = sum(map(int, n.replace('.', '')))
+# print (sum)
 
 # 5. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние
 # между ними в 2D пространстве.
+# Пример:
 
-# from functools import reduce
-# a = list(map(int, input('Введите две координаты первой точки A, через пробел: ').split()))
-# b = list(map(int, input('Введите две координаты второй точки B, через пробел: ').split()))
-# def dot_range(a, b):
-#      rng = reduce(lambda x, y: (x + y)**(1/2), (map(lambda z: (z[1] - z[0])**2, zip(a, b))))
-#      return round(rng, 2)
-# print(dot_range(a, b))
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+from functools import reduce
+a = list(map(int, input('Введите две координаты первой точки A через запятую: ').split(',')))
+b = list(map(int, input('Введите две координаты второй точки B через запятую: ').split(',')))
+def hupot(a, b):
+     res = reduce(lambda x, y: (x + y)**(1/2), (map(lambda z: (z[1] - z[0])**2, zip(a, b))))
+     return round(res,2)
+print(hupot(a, b))
