42e94b52da1ab89dac1d5130b0fdaf53f3368eef
Anna Chirkova <anna.davainis@gmail.com>
2022-12-27 22:10:04 +0500

    do

--- python/HW9/Bot1.py
+++ python/HW9/Bot1.py
@@ -1,52 +1,46 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackContext, Updater
 import datetime
-# from myBot import
+# from myBot import TOKEN
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-
-app = ApplicationBuilder().token("5916744952:AAH2Lnu_ZyUhZyKQeHZKD7mg0DzqJID0RwI").build()
-
-async def hello(update: Update, context: CallbackContext):
+async def hi_command(update: Update, context: CallbackContext):
     log(update, context)
-    await update.message.reply_text(f'Hello, {update.effective_user.first_name}')
+    await update.message.reply_text(f'Hi, {update.effective_user.first_name}')
+
 
 async def time_command(update: Update, context: CallbackContext):
     log(update, context)
     await update.message.reply_text(f'{datetime.datetime.now()}')
 
+
 async def help_command(update: Update, context: CallbackContext):
     log(update, context)
-    await update.message.reply_text(f'/hello\n/time\n/help\n')
+    await update.message.reply_text(f'/hello\n/time\n/help\n/sum\n')
+
 
 async def sum_command(update: Update, context: CallbackContext):
-    # log(update, context)
-    # msg = update.message.text
-    # items = msg.split()
-    # print(msg)
-    # x = int(items[1])
-    # y = int(items[2])
-    # await update.message.reply_text(f'{x} + {y} = {x + y}')
     log(update, context)
     msg = update.message.text
-    print(msg)
     items = msg.split()
-    x = int(input())
-    y = int(input())
-    update.message.reply_text(f'{x} + {y} = {x+y}')
+    print(msg)
+    x = int(items[0])
+    y = int(items[2])
+    await update.message.reply_text(f'{x} + {y} = {x + y}')
+
 def log(update: Update, context: CallbackContext):
     file = open('db.csv', 'a', encoding='utf-8')
     file.write(f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n')
     file.close()
 
 
-app.add_handler(CommandHandler('hello', hello))
+app = ApplicationBuilder().token('5916744952:AAH2Lnu_ZyUhZyKQeHZKD7mg0DzqJID0RwI').build()
+
+app.add_handler(CommandHandler('hi', hi_command))
 app.add_handler(CommandHandler('time', time_command))
 app.add_handler(CommandHandler('help', help_command))
 app.add_handler(CommandHandler('sum', sum_command))
+
 
 print('server start')
 
